71aeb245c856cc88936c522d2b60bd3fef11124f
UMA(ゆーま) <66063731+Yu-Ma28051503@users.noreply.github.com>
2025-11-15 20:51:32 +0900

    learning while, for loops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -22,4 +22,31 @@ fn main() {
 
     println!("End count: {}", count);
     println!("Hello, world!");
+
+    /* while loop */
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    /* for loop */
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("a: {}", element);
+    }
+
+    for index in (0..5).rev() {
+        println!("a[{}]: {}", index, a[index]);
+    }
+
+    for num in (1..4).rev() {
+        println!("{}!", num);
+    }
+
+    println!("LIFTOFF!!!");
 }
